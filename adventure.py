8f95f6b50ba0ae4b0e1d5375918d55dc0bef718f
PJ Johnson <39510645+Phillyfan625@users.noreply.github.com>
2019-06-25 14:23:16 -0400

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,45 @@
+import random
+import sys
+import time
+
+
+print("Our Journey Begins")
+
+name = input("What is your name? \n")
+
+print("Lets go forth our adventure " + name)
+answer = input("You see a pathway in the distance, (left or right)") 
+if answer == ("left"):
+    print("cool")
+    answer = input("Now you see a 100 foot monster, do you run or attack?")
+    if answer == ("run"):
+        print("ouch that sucker killed you")
+        
+    else:
+        print("Well there are good and bad news")
+        print("Good news: the monster is gone")
+        print("bad news: your fingers are gone")
+    
+else:
+    print("Not cool")
+    
+
+    
+     
+   
+
+
+    
+
+    
+        
+
+
+
+
+    
+
+
+            
+
+
